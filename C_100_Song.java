073024e4a4961dd0e9c1383b04e6282d59947ed1
yuji421 <ey130421@gmail.com>
2025-07-17 10:30:49 +0900

    Initial commit
    練習です

---
+++ C_100_Song.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class C_100_Song {
+    public static void main(String[] args) {   
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int S = sc.nextInt();
+        
+        //総持ち時間を秒に変換
+        int totalTime = M*60 + S;
+        
+        //曲の長さを秒で保持
+        int[] songs = new int[N];
+        for(int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            
+            int t = x*60 + y;
+            songs[i] = t;
+        }
+        
+        //短い順にソート
+        Arrays.sort(songs);
+
+        int count = 0;
+        int usedTime = 0;
+
+        //曲をできるだけ多く入れる
+        for(int time : songs) {
+            if(usedTime + time <= totalTime) {
+                usedTime += time;
+                count++;
+            } else {
+                break;
+            }
+        }
+        
+        System.out.println(count);
+
+        sc.close();
+    }
+}
